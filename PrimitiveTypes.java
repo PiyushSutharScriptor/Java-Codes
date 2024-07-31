25b5db3db8766b3b8d9f96b50c47c73eb8af9a8b
Piyush <piyushsuthar6300@gmail.com>
2024-07-31 21:52:20 +0530

    Primitive Types

---
+++ PrimitiveTypes.java
@@ -0,0 +1,22 @@
+public class PrimitiveTypes{
+    public static void main(String[] args){
+        short a = 50;
+        long b = 1000;
+        int c = 90500;
+        float d = 50.56f;
+        double e = 599.987d;
+        char f = 'P';
+        String g = "Java Programming";
+        boolean h = true;
+
+        System.out.println("short : " + a);
+        System.out.println("long : " + b);
+        System.out.println("int : " + c);
+        System.out.println("float : " + d);
+        System.out.println("double : " + e);
+        System.out.println("char : " + f);
+        System.out.println("String : " + g);
+        System.out.println("boolean : " + h);
+
+    }
+}
