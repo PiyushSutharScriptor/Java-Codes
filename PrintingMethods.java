34e4f524430429fc6ad458c7701bf3fb61be9b5a
Piyush <piyushsuthar6300@gmail.com>
2024-07-31 21:48:49 +0530

    Java Printing Methods

---
+++ PrintingMethods.java
@@ -0,0 +1,14 @@
+public class PrintingMethods{
+    public static void main(String[] args){
+        String name = "Java Programming";
+        //println
+        System.out.println(name);
+        //print
+        System.out.print(name);
+        //printf
+        System.out.printf(name);
+        //format
+        System.out.format(name);
+
+    }
+} 
