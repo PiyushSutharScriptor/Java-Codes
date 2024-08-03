3fb9fe522dd4e52755c425bb7dc6b8dc2b46c917
Piyush <piyushsuthar6300@gmail.com>
2024-08-03 10:53:57 +0530

    Escape Sequence Types

---
+++ EscapeSequence.java
@@ -0,0 +1,17 @@
+public class EscapeSequence {
+    public static void main(String[] args){
+        //=> \t
+        System.out.println("Java\tProgramming");
+        //=> \n
+        System.out.println("Java\nProgramming");
+        //=> \b
+        System.out.println("Java \bProgramming");
+        //=> \\
+        System.out.println("Java\\Programming");
+        //=> \'
+        System.out.println("Java\'Programming");
+        //=> \" 
+        System.out.println("Java\"Programming");
+
+    }
+}
