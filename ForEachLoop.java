925f562dff39747996a042683f6a9a604d307dc1
Piyush <piyushsuthar6300@gmail.com>
2024-08-11 18:43:16 +0530

    For Each Loop

---
+++ ForEachLoop.java
@@ -0,0 +1,8 @@
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int arr[] = {5,7,11,13,17,19};
+        for(int element:arr){
+            System.out.println(element);
+        }
+    }
+}
